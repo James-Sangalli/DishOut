fe607b09f793ac4c2b7048880502c7c932a6ac3d
James Sangalli <j.l.sangalli@gmail.com>
2016-05-24 11:50:24 +1200

    update callback url for facebook

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  callbackURL: process.env.NODE_ENV === "production" ? "https://frozen-atoll-63358.herokuapp.com/auth/facebook/callback" : "http://localhost:8080/auth/facebook/callback"
+  callbackURL: process.env.NODE_ENV === "production" ? "http://dishout-potluck.herokuapp.com/auth/facebook/callback" : "http://localhost:8080/auth/facebook/callback"
 }
